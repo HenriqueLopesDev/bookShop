055a5777d31f246305454139e3c7dffe359c0d24
Henrique Lopes <henriquebonfanti2018@gmail.com>
2024-03-31 23:48:07 -0300

    feat: create search books design

--- frontend/app/livros/page.tsx
+++ frontend/app/livros/page.tsx
@@ -1,4 +1,5 @@
 import { BreadCrumb } from '@/components'
+import { MagnifyingGlass, Funnel } from '@phosphor-icons/react/dist/ssr'
 
 export default function Livros() {
   return (
@@ -18,14 +19,23 @@ export default function Livros() {
           <button>Novo livro</button>
         </div>
       </header>
-      <div className="bg-neutral-200 bg-[#e8e8e8] py-3 px-4 rounded-md h-[calc(100vh-196px)]">
-        <div>
-          <form>
-            <input type="text" placeholder="Nome do livro" />
-            <button />
+      <div className="bg-neutral-200 bg-[#e9e9e9] py-3 px-4 rounded-md h-[calc(100vh-196px)]">
+        <div className="flex gap-3">
+          <form className="flex w-full">
+            <input
+              type="text"
+              placeholder="Buscar por livros"
+              className="w-full rounded-l-lg py-1 px-3 outline-none border-y border-l border-neutral-300 focus:border-blue-600 duration-300 font-poppins text-base text-neutral-500 placeholder:font-poppins placeholder:text-sm placeholder:font-medium"
+            />
+            <button className="bg-blue-700 rounded-r-lg py-1 px-3 hover:bg-blue-600 duration-500">
+              <MagnifyingGlass size={16} weight="bold" color="#fff" />
+            </button>
           </form>
+          <button className="bg-blue-700 py-1 px-3 rounded-md flex gap-2 items-center hover:bg-blue-600 duration-500">
+            <span className="text-white font-medium font-poppins">Filtros</span>
+            <Funnel size={20} weight="bold" color="#fff" />
+          </button>
         </div>
-        <button>Filtros</button>
       </div>
     </div>
   )
