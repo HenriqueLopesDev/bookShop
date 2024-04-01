86b26db7f74aa5e72ec1911aab93057123e921dd
Henrique Lopes <henriquebonfanti2018@gmail.com>
2024-03-31 21:02:52 -0300

    feat: create Breadcrumb component

--- frontend/components/breadCrumb/Breadcrumb.tsx
+++ frontend/components/breadCrumb/Breadcrumb.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import { CaretRight } from '@phosphor-icons/react/dist/ssr'
 
 interface breadCrumbRoute {
   path?: string
   name: string
+  description?: string
 }
 
 interface breadCrumbProps {
@@ -10,20 +12,41 @@ interface breadCrumbProps {
 }
 
 export default function BreadCrumb({ breadcrumbs }: breadCrumbProps) {
+  const isLastBreadcrumb = (currentIndex: number): boolean =>
+    currentIndex === breadcrumbs.length - 1
+
   return (
     <div className="font-poppins flex items-center gap-1">
-      <Link href="/" className="text-neutral-500">
+      <Link
+        href="/"
+        className="text-neutral-500 hover:text-blue-500 duration-500 font-medium text-sm"
+        title="Ir para a página inicial"
+      >
         Início
       </Link>
-      {breadcrumbs.map((breadcrumb) => (
-        <Link
-          key={breadcrumb.path}
-          href={breadcrumb.path ?? ''}
-          className="text-neutral-400"
-        >
-          {breadcrumb.name}
-        </Link>
-      ))}
+      <CaretRight size={12} weight="bold" color="#525252" />
+      {breadcrumbs.map((breadcrumb, index) => {
+        return isLastBreadcrumb(index) ? (
+          <span
+            key={breadcrumb.path}
+            className="text-neutral-400 font-medium text-sm"
+          >
+            {breadcrumb.name}
+          </span>
+        ) : (
+          <>
+            <Link
+              key={breadcrumb.path}
+              href={breadcrumb.path ?? ''}
+              title={breadcrumb.description ?? ''}
+              className="text-neutral-400 hover:text-blue-500 duration-500 font-medium text-sm"
+            >
+              {breadcrumb.name}
+            </Link>
+            <CaretRight size={12} weight="bold" color="#525252" />
+          </>
+        )
+      })}
     </div>
   )
 }
