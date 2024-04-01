8d058573a7bc9c9be4eda46312e05e2d1fc6698a
Henrique Lopes <henriquebonfanti2018@gmail.com>
2024-04-01 00:11:38 -0300

    feat: implement new book button style

--- frontend/app/livros/page.tsx
+++ frontend/app/livros/page.tsx
@@ -1,5 +1,9 @@
 import { BreadCrumb } from '@/components'
-import { MagnifyingGlass, Funnel } from '@phosphor-icons/react/dist/ssr'
+import {
+  MagnifyingGlass,
+  Funnel,
+  PlusCircle,
+} from '@phosphor-icons/react/dist/ssr'
 
 export default function Livros() {
   return (
@@ -16,7 +20,10 @@ export default function Livros() {
           <h1 className="font-semibold font-mont text-3xl text-blue-800">
             Livros
           </h1>
-          <button>Novo livro</button>
+          <button className="text-white bg-blue-600 py-2 px-4 rounded-lg flex gap-2 items-center hover:bg-blue-500 hover:text-neutral-100 duration-500">
+            <span className="font-medium font-poppins">Novo livro</span>
+            <PlusCircle size={20} weight="bold" />
+          </button>
         </div>
       </header>
       <div className="bg-neutral-200 bg-[#e9e9e9] py-3 px-4 rounded-md h-[calc(100vh-196px)]">
@@ -27,13 +34,13 @@ export default function Livros() {
               placeholder="Buscar por livros"
               className="w-full rounded-l-lg py-1 px-3 outline-none border-y border-l border-neutral-300 focus:border-blue-600 duration-300 font-poppins text-base text-neutral-500 placeholder:font-poppins placeholder:text-sm placeholder:font-medium"
             />
-            <button className="bg-blue-700 rounded-r-lg py-1 px-3 hover:bg-blue-600 duration-500">
-              <MagnifyingGlass size={16} weight="bold" color="#fff" />
+            <button className="text-white bg-blue-600 rounded-r-lg py-1 px-3 hover:bg-blue-500 hover:text-neutral-100 duration-500">
+              <MagnifyingGlass size={16} weight="bold" />
             </button>
           </form>
-          <button className="bg-blue-700 py-1 px-3 rounded-md flex gap-2 items-center hover:bg-blue-600 duration-500">
-            <span className="text-white font-medium font-poppins">Filtros</span>
-            <Funnel size={20} weight="bold" color="#fff" />
+          <button className="text-white bg-blue-600 py-1 px-3 rounded-md flex gap-2 items-center hover:bg-blue-500 hover:text-neutral-100 duration-500">
+            <span className=" font-medium font-poppins">Filtros</span>
+            <Funnel size={20} weight="bold" />
           </button>
         </div>
       </div>
